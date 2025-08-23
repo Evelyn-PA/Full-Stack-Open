7f3c54148b2b56741ea4a40ca440776a2658acbb
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-08-23 10:53:50 +0300

    Done the excercise of part1.a

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,21 +6,34 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const sum = exercises1+exercises2+exercises3
+
+  //Header component
   const Header = (props) => {
     return (
       <h1>{props.course}</h1>
     )
   }
+
+  //Content component
   const Content = (props) => {
     return (
       <>
         <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2}{props.exercises2}</p>
-        <p>{props.part3}{props.exercises3}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
       </>
 
     )
   }
+
+  //Total component
+  const Total = (props) =>{
+    return (
+      <p>Number of exercises {sum}</p>
+    )
+  }
+
   return (
     <div>
       <Header course={course} />
@@ -29,7 +42,9 @@ const App = () => {
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total
+      sum = {sum}
+      />
     </div>
   )
 }
