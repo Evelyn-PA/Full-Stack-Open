a309d2ed7bb7f30bc34f43b2bebc39cea0e1486d
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-09-02 15:05:18 +0300

    Complete task 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+const Button = (props)=>(
+  <button onClick={props.onClick}>{props.text}</button>
+)
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <Button onClick={()=> setGood(good+1)} text ="Good"/>
+      <Button onClick={()=> setNeutral(neutral+1)} text ="Neutral"/>
+      <Button onClick={()=> setBad(bad+1)} text ="Bad"/>
+
+      <h1>Statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+
     </div>
   )
 }
