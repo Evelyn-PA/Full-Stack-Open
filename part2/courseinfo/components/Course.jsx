e3ac1cbb4a9a8e071be6b407f887fc97e072b3f3
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-01-12 22:10:12 +0200

    Complete task 2.4

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -13,33 +13,26 @@ export default function Course({ course }) {
         )
     }
     //Content component
-    const Content = (props) => {
-        return (
-            <>
-                <Part name={props.part[0].name} exercises={props.part[0].exercises} />
-                <Part name={props.part[1].name} exercises={props.part[1].exercises} />
-                <Part name={props.part[2].name} exercises={props.part[2].exercises} />
-            </>
-
-        )
-    }
-    const Total2 = course.parts.reduce((s, p) => s+p.exercises, 0)
-    const Total = (props) => {
-        return (
-            <p><b>total of {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises} exercises</b></p>
-        )
+    const Content = ({ parts }) => (
+        <>
+            {parts.map(part => (
+                <Part key={part.id} name={part.name} exercises={part.exercises} />
+            ))}</>
+    )
+    // const Total2 = course.parts.reduce((s, p) => s + p.exercises, 0)
+    const Total = ({ parts }) => {
+        const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+        return <p><b>total of {total} exercises</b></p>
     }
     return (
         <div>
             <Header course={course} />
             <Content
-                part={course.parts}
+                parts={course.parts}
             />
             <Total
-                part={course.parts}
+                parts={course.parts}
             />
-
-            <p>Total {Total2}</p>
         </div>
     )
 }
