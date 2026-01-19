01ecfd2eade71fefc13e9fed90f880faaf9a9136
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-01-20 00:00:25 +0200

    Feat: Complete Task 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,40 +11,47 @@ const App = () => {
   const [newPhone, setNewPhone] = useState('')
   const [filterPerson, setFilterPerson] = useState("")
 
-//Apply the useEffect to fetch the data
-useEffect(() => {
-  axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    })
-}, [])
+  //Apply the useEffect to fetch the data
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
-//Filtering the data
-const filteredPerson = persons.filter((person) => person.name.toLowerCase().includes(filterPerson.trim().toLowerCase()))
+  //Filtering the data
+  const filteredPerson = persons.filter((person) => person.name && person.name.toLowerCase().includes(filterPerson.trim().toLowerCase()))
 
-// Handlers
-const onNameChange = (e) => setNewName(e.target.value)
-const onPhoneChange = (e) => setNewPhone(e.target.value)
-const onFilterChange = (e) => setFilterPerson(e.target.value)
+  // Handlers
+  const onNameChange = (e) => setNewName(e.target.value)
+  const onPhoneChange = (e) => setNewPhone(e.target.value)
+  const onFilterChange = (e) => setFilterPerson(e.target.value)
 
-const addPerson = (event) => {
-  event.preventDefault()
-  persons.some(person => person.name === newName) ? window.alert(`${newName} is already added to phonebook`) : setPersons(persons.concat({ name: newName, number: newPhone }))
-  setNewName("")
-  setNewPhone("")
-}
+  const addPerson = (event) => {
+    event.preventDefault()
+    persons.some(person => person.name === newName) ? window.alert(`${newName} is already added to phonebook`) : setPersons(persons.concat({ name: newName, number: newPhone }))
+    const newPerson = { name: newName, number: newPhone }
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName("")
+        setNewPhone("")
+      })
 
-return (
-  <div>
-    <h1>Phonebook</h1>
-    <Filter persons={persons} onFilterChange={onFilterChange} />
-    <h1>Add a new</h1>
-    <PersonForm addPerson={addPerson} onNameChange={onNameChange} onPhoneChange={onPhoneChange} newName={newName} newPhone={newPhone} />
-    <h2>Numbers</h2>
-    <Person filteredPerson={filteredPerson} />
-  </div>
-)
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter persons={persons} onFilterChange={onFilterChange} />
+      <h1>Add a new</h1>
+      <PersonForm addPerson={addPerson} onNameChange={onNameChange} onPhoneChange={onPhoneChange} newName={newName} newPhone={newPhone} />
+      <h2>Numbers</h2>
+      <Person filteredPerson={filteredPerson} />
+    </div>
+  )
 }
 
 export default App
