7738f03970953a418416f0e1f226a7ab6ac77763
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-01-19 23:46:17 +0200

    Feat: Complete Task 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './component/Filter'
 import PersonForm from './component/PersonForm'
 import Person from './component/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filterPerson, setFilterPerson] = useState("")
 
-  //Filtering the data
-  const filteredPerson = persons.filter((person) => person.name.toLowerCase().includes(filterPerson.trim().toLowerCase()))
+//Apply the useEffect to fetch the data
+useEffect(() => {
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+}, [])
 
-  // Handlers
-  const onNameChange = (e) => setNewName(e.target.value)
-  const onPhoneChange = (e) => setNewPhone(e.target.value)
-  const onFilterChange = (e) => setFilterPerson(e.target.value)
+//Filtering the data
+const filteredPerson = persons.filter((person) => person.name.toLowerCase().includes(filterPerson.trim().toLowerCase()))
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    persons.some(person => person.name === newName) ? window.alert(`${newName} is already added to phonebook`) : setPersons(persons.concat({ name: newName, number: newPhone }))
-    setNewName("")
-    setNewPhone("")
-  }
+// Handlers
+const onNameChange = (e) => setNewName(e.target.value)
+const onPhoneChange = (e) => setNewPhone(e.target.value)
+const onFilterChange = (e) => setFilterPerson(e.target.value)
 
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Filter persons={persons} onFilterChange={onFilterChange} />
-      <h1>Add a new</h1>
-      <PersonForm addPerson={addPerson} onNameChange={onNameChange} onPhoneChange={onPhoneChange} newName={newName} newPhone={newPhone}/>
-      <h2>Numbers</h2>
-      <Person filteredPerson={filteredPerson}/>
-    </div>
-  )
+const addPerson = (event) => {
+  event.preventDefault()
+  persons.some(person => person.name === newName) ? window.alert(`${newName} is already added to phonebook`) : setPersons(persons.concat({ name: newName, number: newPhone }))
+  setNewName("")
+  setNewPhone("")
+}
+
+return (
+  <div>
+    <h1>Phonebook</h1>
+    <Filter persons={persons} onFilterChange={onFilterChange} />
+    <h1>Add a new</h1>
+    <PersonForm addPerson={addPerson} onNameChange={onNameChange} onPhoneChange={onPhoneChange} newName={newName} newPhone={newPhone} />
+    <h2>Numbers</h2>
+    <Person filteredPerson={filteredPerson} />
+  </div>
+)
 }
 
 export default App
