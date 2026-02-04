dba65559ce62baa1f3be2f5b62f49a4a4e11e6c4
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-02-04 22:13:38 +0200

    Feat: Learn some HTTP method and interact with POSTMAN

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -7,7 +8,7 @@ let notes = [
         important: true
     },
     {
-        id: "2",
+        id: "4",
         content: "Browser can execute only JavaScript",
         important: false
     },
@@ -17,12 +18,22 @@ let notes = [
         important: true
     }
 ]
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { "content-Type": "application/json" })
-    res.end(JSON.stringify(notes))
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World!</h1>")
 })
 
-const PORT = 3002
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
-console.log("http://localhost:3002")
+app.get('/api/notes',(req, res) =>{
+    res.json(notes)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
