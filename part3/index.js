6905be2b0115492780666db801ed92ecc5ab5966
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-02-05 16:54:55 +0200

    Feat: Complete task 3.2

--- part3/index.js
+++ part3/index.js
@@ -3,36 +3,43 @@ const app = express()
 
 //The Data
 const person = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-app.get("/", (req, res)=>{
-    res.send("<h1>Hello Welcome to my data</h1>")
+app.get("/info", (req, res) => {
+    const total = person.length
+    const CurrentTime = new Date()
+    res.send(
+        `<p>Phonebook has info for ${total} people </p>
+        <p>${CurrentTime}</p>
+       `
+    )
 })
 
-app.get("/api/persons", (req, res) =>{
+app.get("/api/persons", (req, res) => {
     res.json(person)
 })
 
 PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Server running at http://localhost:${PORT}`)})
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
