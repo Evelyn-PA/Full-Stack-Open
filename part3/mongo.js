1cea18992c74d3f033a7c2259f1dd66652c48af2
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-02-14 01:19:41 +0200

    Feat: Complete task 3.12

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
+if (process.argv.length < 3) {
     console.log("give password as argument & data")
     process.exit(1)
 }
 
+
+
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
-const url = `mongodb+srv://phuonganhnguyen6165_db_user:${password}@cluster0.qp7sjdp.mongodb.net/?appName=Cluster0`
+const url = `mongodb+srv://phuonganhnguyen6165_db_user:${password}@cluster0.qp7sjdp.mongodb.net/PhoneBook?appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
@@ -20,20 +22,28 @@ const phoneBookSchema = new mongoose.Schema({
     number: String
 })
 
-const phoneBook = mongoose.model("phoneBoook", phoneBookSchema)
+const phoneBook = mongoose.model("phoneBook", phoneBookSchema)
 
-const phonebook = new phoneBook({
-    id: "1",
-    name: name,
-    number: number
-})
 
-phonebook.save().then(result => {
-    if (name.includes(" ")) {
-        console.log(`Added "${name}" and ${number} to phonebook`)
-    }
-    else {
-    console.log(`Added ${name} and ${number} to phonebook`)
+
+if (process.argv.length === 3) {
+    console.log("Phonebook:")
+    phoneBook.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
 }
-mongoose.connection.close()
-})
+else if (process.argv.length === 5) {
+    const phonebook = new phoneBook({
+        id: "1",
+        name: name,
+        number: number
+    })
+
+    phonebook.save().then(result => {
+        console.log(`Added ${name} and ${number} to phonebook`)
+        mongoose.connection.close()
+    });
+}
