5c454200db97f5a451f610eeb7b4a4fc459962f1
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2026-02-13 23:13:46 +0200

    Feat: Add initial test

---
+++ part3/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+    console.log("give password as argument & data")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+const url = `mongodb+srv://phuonganhnguyen6165_db_user:${password}@cluster0.qp7sjdp.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })  //Always use IPv4
+
+const phoneBookSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    number: String
+})
+
+const phoneBook = mongoose.model("phoneBoook", phoneBookSchema)
+
+const phonebook = new phoneBook({
+    id: "1",
+    name: name,
+    number: number
+})
+
+phonebook.save().then(result => {
+    if (name.includes(" ")) {
+        console.log(`Added "${name}" and ${number} to phonebook`)
+    }
+    else {
+    console.log(`Added ${name} and ${number} to phonebook`)
+}
+mongoose.connection.close()
+})
